6fc4c3ea3b67f8fd3120d4bbeb20c11e06d6bef6
ganeshkousik <ganeshkousik313@gmail.com>
2021-05-23 14:49:23 +0530

    dropdown

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,40 @@
 import React, { useState, useMemo } from 'react'
-import Select from 'react-select'
-import countryList from 'react-select-country-list'
-
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+}));
 export default function Navbar(){
-    const [value, setValue] = useState('')
-    const options = useMemo(() => countryList().getData(), [])
+    const classes = useStyles();
+    const [age, setAge] = React.useState('');
 
-    const changeHandler = value => {
-        setValue(value)
-    }
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
     return(
         <div className="navbar">
             <h1 className="text-white font-weight-bold">
                 COVID-19
             </h1>
             <div>
-                <Select options={options} value={value} onChange={changeHandler} />
+                <FormControl className={classes.formControl}>
+                    <Select value={age} onChange={handleChange} displayEmpty className={classes.selectEmpty} inputProps={{ 'aria-label': 'Without label' }}>
+                        <MenuItem value="">
+                            <em>World</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
             </div>
         </div>
     )
