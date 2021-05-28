2a88a6d69e528b4e376573527d7783f7beb9990f
ganeshkousik <ganeshkousik313@gmail.com>
2021-05-28 23:32:26 +0530

    chart

--- src/components/Dashboard/Graphicalview.js
+++ src/components/Dashboard/Graphicalview.js
@@ -1,59 +1,98 @@
-import { set } from 'mongoose';
 import react, {useState} from 'react'
-import { Bar } from 'react-chartjs-2';
-export default function Graphicalview(props){
-    const data = {
-        labels: ['Total Cases','Total Active Cases','Recovered Cases','Total Deaths'],
-        datasets: [
-            {
-                label: "cases",
-                data: [props.covid_cases.cases,props.covid_cases.active,props.covid_cases.recovered,props.covid_cases.deaths],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1,
-            },
-        ],
-    };
-    const options = {
-        scales: {
-            yAxes: [
-                {
-                    ticks: {
-                        beginAtZero: true,
+import ReactApexChart from "react-apexcharts";
+export default class Graphicalview extends react.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+          
+            series: [{
+                data: [this.props.covid_cases.cases,this.props.covid_cases.active,this.props.covid_cases.recovered,this.props.covid_cases.deaths],
+            }],
+            options: {
+                responsive: [{
+                    breakpoint: undefined,
+                    options: {},
+                }],                
+                grid: {
+                    show: true,
+                    yaxis: {
+                        lines: {
+                            show: false
+                        }
+                    }, 
+                    xaxis: {
+                        lines: {
+                            show: false
+                        }
+                    }, 
+                },    
+                chart: {
+                    height: 550,
+                    type: 'bar',
+                },
+                colors: ['#1E90FF','#00FF7F','#FFD700','#FF0000'],
+                plotOptions: {
+                    bar: {
+                        columnWidth: '45%',
+                        distributed: true,
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                legend: {
+                    show: true,
+                    position: "top",
+                    labels: {
+                        style: {
+                            colors: ["#fff","#fff","#fff","#fff"]
+                        },
+                        useSeriesColors: true
+                    },
+                    itemMargin: {
+                        horizontal: 35,
+                        vertical: 0
                     },
                 },
-            ],
-        },
-    };
-    return(
-        <div className="graphical-view">
-            <div className="d-flex align-items-center close-graph justify-content-between">
-                <h1 className="text-white font-weight-bold">
-                    COVID-19
-                </h1>
-                <div>
-                    <div className="d-block pull-right mr-3" onClick={props.closeGraphical}>
-                        <i className="fa fa-times-circle text-white fs-26"></i>
+                xaxis: {
+                    categories: [
+                        ['Total Cases'],['Total Active Cases'],['Total Recovered Cases'],['Total Deaths']
+                    ],
+                    labels: {
+                        fontSize: '12px',
+                        style: {
+                            colors: ["#fff","#fff","#fff","#fff"]
+                        },
+                    }
+                },
+                yaxis: {
+                    labels: {
+                        style: {
+                            colors: ["#fff","#fff","#fff","#fff"]
+                        },
+                    }
+                }
+            },  
+        };
+    }
+    render(){
+        return(
+            <div className="graphical-view">
+                <div className="d-flex align-items-center close-graph justify-content-between">
+                    <h1 className="text-white font-weight-bold">
+                        COVID-19
+                    </h1>
+                    <div>
+                        <div className="d-block pull-right mr-3" onClick={this.props.closeGraphical}>
+                            <i className="fa fa-times-circle text-white fs-26"></i>
+                        </div>
                     </div>
                 </div>
+                <div id="chart">
+                    <ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={550} />
+                </div>
             </div>
-            <div>
-                <Bar width={100} height={80} options={{ maintainAspectRatio: false }} data={data} options={options} />
-            </div>
-        </div>
-    )
+        )            
+    }
 }
