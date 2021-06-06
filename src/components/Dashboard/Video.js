df9646116f8cc247612db26fc982ee2e78960d37
ganeshkousik <ganeshkousik313@gmail.com>
2021-06-06 22:05:37 +0530

    mp4

--- src/components/Dashboard/Video.js
+++ src/components/Dashboard/Video.js
@@ -25,7 +25,7 @@ export default class Video extends React.Component {
         return (
             <div className="video">
                 <video className="w-100" autoPlay={true} muted playsinline loop id="myVideo">
-                    <source src="https://cdn.videvo.net/videvo_files/video/free/2020-12/small_watermarked/201211%20_01_Covid%20Vaccine_4k_011_preview.webm" type="video/webm" />
+                    <source src="https://storage.cloudconvert.com/tasks/ce97f0b4-2663-4710-bdb2-c3222e947208/201211%2020_01_Covid%2020Vaccine_4k_011_preview.mp4?AWSAccessKeyId=cloudconvert-production&Expires=1623083660&Signature=sr5dyXjeRb8konDYvOpwVebykL4%3D&response-content-disposition=inline%3B%20filename%3D%22201211%2020_01_Covid%2020Vaccine_4k_011_preview.mp4%22&response-content-type=video%2Fmp4" type="video/mp4" />
                 </video>
             </div>    
         );
